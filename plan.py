6385efb76e2567fd76f76e5c9c573534db0b5271
astoorangi <astoorangi@outlook.com>
2017-11-10 17:23:11 +0100

    First commit

---
+++ plan.py
@@ -0,0 +1,137 @@
+from urllib.request import urlopen  # Für Download
+from bs4 import BeautifulSoup       # Zum Parsen
+import time                         # Zum Finden des Jahres einer Vertretung
+import json                         # Zum konvertieren in Json
+
+class updateWeek():
+    def __init__(self,url,datei,file='data.json'):    # 1 Instanz == Eine Url
+        self.url = url
+        self.datei = datei
+        self.file = file
+
+    def createUrl(self,week):                       # Erstellt URL aus self.url, self.datei und angegebener Woche
+        fullUrl = self.url + str(week) + self.datei
+        return(fullUrl)
+
+    def downloadWeek(self,week):                    # Downloadet eine Woche
+        html = urlopen(self.createUrl(week)).read().decode('latin2')
+        return(html)
+
+    def findNone(self,content):
+        if content == "---" or content == "&nbsp;" or content == "\xa0":    # Zeichen, die "Kein Inhalt" bedeuten
+            return(None)                                        # Durch None ersetzen
+        else:
+            return(content)                                         # Wenn Inhalt = gebe wieder Inhalt zurück
+
+    def createTime(self,date,title): # Jeder 31.7. liegt in Sommerferien --> < 2. Halb | > 1. Halb
+        daymonth = time.strptime(date,'%d.%m.')             # Fasse Tag & Monat in daymonth zusammen
+        month = int(time.strftime('%m',daymonth))
+        currentyear = int(time.strftime('%y'))               # currentyear = Aktuelles Jahr
+
+        for year in range(currentyear-2,currentyear+1):
+            if title.count(str(year) + '/' + str(year+1)) > 0 or title.count(str(year) + '/20' + str(year+1)) > 0:
+                if month <= 7:
+                    return(date + '20' + str(year + 1))
+                elif month > 7:
+                    return(date + '20' + str(year))
+
+    def parseHtml(self,html):                       # Parsen des Downloads
+        with open(self.file) as file: # Importiere data.json
+            data = json.load(file)      # Als data
+
+        soup = BeautifulSoup(html,"html.parser")
+        title = soup.title.string
+        tablesraw = soup.findAll('table')
+        
+        motd = None
+        date = None
+        for table in tablesraw:                 # Jede Tabelle
+            if table.get('class') != ['subst']: # Wenn Tabelle keine Vertretungen beinhaltet => motd
+                motd = table.get_text()          # Schreibe Inhalt der Tabelle in motd
+                motd
+                motd = motd.replace('\nNachrichten zum Tag\n','')
+                motdsplit = []
+                for m in motd:
+                    motdsplit.append(m)
+                motdsplit.pop()
+                motd = ""
+                for m in motdsplit:
+                    motd += m
+                continue                            # Aktuelle Tabelle zu Ende bearbeitet --> Abbruch der Verarbeitung
+            rowsraw = table.findAll('tr')       # rowsraw = Liste mit Tabellen-Reihen
+            if len(rowsraw) == 1:               # Wenn Anzahl Reihen = 1, weil keine Vertretungen verfügbar
+                continue                            # Abbruch, damit keine unnötigen Informationen in json
+            vertretungen=[]                     # Erstelle Liste für die Vertretungen einer Tabellen
+            for row in rowsraw:                 # Jede Reihe in der Tabelle
+                colsraw = row.findAll('td')         # colsraw = Liste mit allen Zellen in der Reihe
+                if len(colsraw)==0:                 # Wenn Anzahl der Zelle == 0: Abbruch, damit keine leeren Reihen eingetragen werden ('th's)
+                    continue
+                c = 0                           # c = 0, wird durchgezählt um Inhalt Zelle herauszufinden, wird für neue Reihe hiermit resettet
+                vertretung = {}                 # dict für einzelene Vertretung
+                for col in colsraw:             # Jede Zelle
+                    if c == 0:                          # Stunde
+                        stunden = col.string
+                        stunden = stunden.replace("- ","")
+                        stunden = stunden.split()
+                        vertretung["stunden"] = stunden
+                    elif c == 1:                        # Datum
+                        if self.findNone(col.string) == col.string:
+                            date = col.string
+                    elif c == 2:
+                        vertretung["vertreter"] = self.findNone(col.string)
+                    elif c == 3:                        # Klasse(n)
+                        klasse = col.string
+                        klasse = klasse.replace(",","")
+                        klasse = klasse.split()
+                        vertretung["klasse"] = klasse
+                    elif c == 4:                        # Art der Vertretung(Raumvertretung, Entfall, Vertretung)
+                        vertretung["art"] = self.findNone(col.string)
+                    elif c == 5:                        # Altes Fach
+                        vertretung["fachalt"] = self.findNone(col.string)
+                    elif c == 6:                        # Neues Fach
+                        vertretung["fach"] = self.findNone(col.string)
+                    elif c == 7:                        # Dauerhaft leere Spalte -> wird übersprungen
+                        None
+                    elif c == 8:                        # Text zur Vertretung
+                        vertretung["text"] = self.findNone(col.string)
+                    elif c == 9:                        # Raum der Vertretung
+                        vertretung["raum"] = self.findNone(col.string)
+                    else:                               # Sollte c > Anzahl der Spalten werden: Warnung
+                        print("Fehler: c > 9")
+                    c += 1                              # c+=1, damit nächste Spalte/Zelle
+
+                if vertretung["art"] == None:
+                    #print(vertretungen[-1])
+                    vertretungen[-1]["text"] += " " + vertretung["text"]
+                    continue
+
+                vertretungen.append(vertretung)     # Nach allen Zellen: Füge Vertretung in Liste Vertretungen zusammen
+                olddate = date                      # Altes Datum = Datum, für nächste Runde
+            daycontent = {"motd":motd,"vertretungen":vertretungen}  # Füge Motd und Liste der Vertretungen des Tages in daycontent zusammen
+            motd = None                             # Leere motd, damit gleich nicht mehr vorhanden
+            date = self.createTime(date,title)      # Füge mit Funktion date Jahr zum Datum hinzu
+            data["days"][date] = {}                 # Leere bisherigen dict zum Datum
+            data["days"][date] = daycontent         # Fülle dict des Datums mit daycontent
+
+        with open(self.file,'w') as file:         # Öffne data.json
+            file.write(json.dumps(data,indent=None))   # Speichere in data.json ab, indent = Einrückung (None = Alles eine Zeile)
+                    
+
+    def update(self,week):                          # Starten eines Updates einer Woche
+        html = self.downloadWeek(week)
+        self.parseHtml(html)
+
+
+class get():
+    def __init__(self,file="data.json"):
+        self.file = file
+    
+    def search(self,date,isklasse):
+        with open(self.file) as file:   # Importiere data.json
+            data = json.load(file)      # Als data
+        sortedVertretungen = []
+        for vertretung in data["days"][date]["vertretungen"]:
+            for klasse in vertretung["klasse"]:
+                if klasse == isklasse:
+                    sortedVertretungen.append(vertretung)
+        return({date:sortedVertretungen})
---
+++ website/js/plan.js
@@ -0,0 +1,147 @@
+var date = new Date();
+
+function showInfo() {               // Zeige Klasse im Footer
+    $('#infos').append(getCookie("klasse"));
+}
+
+function updateDate(anzahl) {       // Verschiebe Datum um x Tage
+    date.setDate(date.getDate() + anzahl);  // Erhöhe var date um anzahl aus function
+    showDate();         // Rufe functions zum aktualisieren der Ansicht auf
+    showVertretungen();
+}
+
+function showDate() {               // Zeige Datum in nav        
+    var wochentag = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];  // Array zum Übersetzen des Wochentages
+    $('#datum').html(wochentag[date.getDay()] + ', ' + date.toLocaleDateString());  // Schreibe Wochentag und Datum in nav
+}
+
+function showVertretungen() {       // Fordere & Zeige Vertretungen in Tabelle an
+    var url = "api/" + getCookie("urlhash") + "/" + date.toLocaleDateString() + "/" + getCookie("klasse");       // Erstelle URL
+    $('tbody').empty();                                 // Entferne bisherige Vertretungen
+    $.ajax({        // Stelle Ajax
+        url: url
+    })
+        .done(function (data) {
+            console.log(data);
+            var datum = date.toLocaleDateString();          // Konvertiere Datum in tt.mm.yyyy
+            var vertretungen = data[datum];     // Rufe vertretungen in Array "vertretungen"
+
+            if (vertretungen == null) {     // Wenn Vertretunge null = Keine vorhanden
+                $('tbody').append(          // Schreibe Info in Tabelle
+                    '<tr>' +
+                    '<td colspan="8">Keine Daten für den heutigen Tag vorhanden.</td>' +
+                    '</tr>'
+                );
+            }
+
+            else if (vertretungen.length == 0) {    // Wenn Anzahl der Vertretungen 0 = Keine für Auswahl vorhanden
+                $('tbody').append(          // Informiere über keine Vertretungen
+                    '<tr>' +
+                    '<td colspan="8">Keine Vertretungen verfürbar.</td>' +
+                    '</tr>'
+                );
+            }
+
+            else {                                          // Else = Vertretungen für Auswahl vorhanden
+                for (let vertretung of vertretungen) {              // Gehe Vertretunge in for of durch
+                    var cssclass = "";                              // Definiere cssclass
+
+                    if (vertretung.art == "Entfall") {              // Ändere nach Art der Vertretung Hintergrundfarbe der Zeilen
+                        cssclass = " class=\"danger\"";             // Durch ändern der var cssclass
+                    } else if (vertretung.art == "Raum-Vtr.") {
+                        cssclass = " class=\"warning\"";
+                    }
+                    $('tbody').append(                              // Füge Vertretung hinzu
+                        '<tr' + cssclass + '>' +                                                // class für Farbe nach Art
+                        '<td>' + checkPrint(vertretung.stunden.toString().replace(',', ' - ')) + '</td>' +      // Stunde(n)
+                        '<td>' + checkPrint(vertretung.fachalt) + '</td>' +                                     // Altes Fach
+                        '<td>' + checkPrint(vertretung.fach) + '</td>' +                                        // Fach
+                        '<td>' + checkPrint(vertretung.art) + '</td>' +                                         // Art
+                        '<td>' + checkPrint(vertretung.raum) + '</td>' +                                        // Raum
+                        '<td>' + checkPrint(vertretung.vertreter) + '</td>' +                                   // Vertreter
+                        '<td>' + checkPrint(vertretung.text) + '</td>' +                                        // Text
+                        '<td>' + checkPrint(vertretung.klasse.toString().replace(',', ', ')) + '</td>' +        // Klasse(n)
+                        '</tr>'
+                    );
+                };
+            }
+        });
+}
+
+function checkPrint(toprint) {
+    if (toprint !== null) {
+        return(toprint);
+    }
+    else {
+        return("");
+    }
+}
+
+function reset() {                  // Lösche Cookies & öffne Form
+    var dz = new Date(0);       // var dz = 1.1.1970
+    document.cookie = "klasse=; expires=" + dz + "; path=/";    // Lösche Cookie "klasse"
+    document.cookie = "url=; expires=" + dz + "; path=/";       // Lösche Cookie "url"
+    document.cookie = "urlhash=; expires=" + dz + "; path=/";   // Lösche Cookie "urlhash"
+    checkState();               // Zum Anzeigen des Forms zum neuen Definieren der Cookies
+}
+
+function getCookie(cname) {         // Cookie nach Namen abfragen (by w3schools)
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie() {              // Schreibe Cookies aus Form
+    var ablauf = new Date();                // Erstelle var "ablauf" auf aktuellem Datum
+    var zeit = ablauf.getTime() + (365 * 24 * 60 * 60 * 1000);  // var zeit = var ablauf + 1 Jahr
+    ablauf.setTime(zeit);                   // var ablauf = var zeit
+
+    var klasse = $('#klasse')[0].value;     // var klasse = textform "#klasse"
+    var url = $('#url')[0].value;           // var url = textform "#url"
+    var urlhash = sha1(url);                // var urlhash = sha1 von var url
+
+    document.cookie = "klasse=" + klasse + "; expires=" + ablauf.toUTCString() + "; path=/";    // c klasse = var klasse
+    document.cookie = "url=" + url + "; expires=" + ablauf.toUTCString() + "; path=/";          // c url = var url
+    document.cookie = "urlhash=" + urlhash + "; expires=" + ablauf.toUTCString() + "; path=/";  // c urlhash = var urlhash
+
+
+    checkState();                   // Blende Form aus, wenn Cookies gespeichert
+}
+
+function form(state) {              // Zeige Form an / blende aus
+    if (state == true) {    // Formular anzeigen
+        $('#popup').css("display", "block");        // Zeige unterschiedliche DIVs an / nicht an
+        $('#content nav').css("display", "none");
+        $('#content main').css("display", "none");
+        $('#motd').css("display", "none");
+        $.getScript("js/sha1.min.js");              // Importiere sha1 script um urlhash zu erstellen
+    }
+    else {                  // Formular ausblenden
+        $('#popup').css("display", "none");         // Zeige unterschiedliche DIVs an / nicht an
+        $('#content nav').css("display", "block");
+        $('#content main').css("display", "block");
+        $('#motd').css("display", "block");
+        showInfo();             // Aktualisiere Daten // Doppelt????
+        showDate();
+        showVertretungen();
+    }
+}
+
+function checkState() {             // Überprüfe, ob Cookies für Plan vorhanden oder ob Form angezeigt werden soll
+    if (getCookie("klasse") == "" || getCookie("url") == "" || getCookie("urlhash") == "") {
+        form(true);
+    }
+    else {
+        form(false);
+    }
+}
